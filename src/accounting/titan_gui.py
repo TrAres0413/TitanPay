35dd26b90e75ba7e04acc3e488af0332b15169b8
TrAres0413 <ccapps3804@live.com>
2016-06-25 22:06:25 -0400

    Revision

--- src/accounting/titan_gui.py
+++ src/accounting/titan_gui.py
@@ -5,15 +5,14 @@ import tkinter.messagebox
 class TitanGUI:
     def __init__(self):
         self.__first_window = tkinter.Tk()
-        self.__button = tkinter.Button(self.__first_window, text='Process Payroll', command=self.commit_payroll)
-        self.__button.pack()
+        self.__button = tkinter.Button(self.__first_window, text='Process Payroll', fg="green", command=self.commit_payroll)
+        self.__button.pack(side=tkinter.BOTTOM)
         tkinter.mainloop()
 
     def commit_payroll(self):
         self.__second_window = tkinter.Tk()
-        self.__button = tkinter.Button(self.__second_window, text='Commit Payroll', command=self.finish_payroll)
+        self.__button = tkinter.Button(self.__second_window, text='Commit Payroll', fg="red", command=self.finish_payroll)
         self.__button.pack()
-        tkinter.mainloop()
 
     def finish_payroll(self):
         tkinter.messagebox.showinfo('Processing Payroll')

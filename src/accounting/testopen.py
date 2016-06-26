ccf105e1f5650776f4e0190dc6d2b7e64d611267
TrAres0413 <ccapps3804@live.com>
2016-06-26 13:52:55 -0400

    Revision

--- src/accounting/testopen.py
+++ src/accounting/testopen.py
@@ -1,9 +1,8 @@
 import csv
-
 from src.accounting.employee import Employee
 
 
-class testOpen:
+class TestOpen:
     employee = {}
     with open('hourly_employees.csv', 'r') as hourly_file:
         for line in hourly_file:
@@ -13,28 +12,29 @@ class testOpen:
                 emp = Employee(employee_id, last, first, rate, pay_meth)
                 employee.update(eval(hourly_file, emp))
         with open('salaried_employees.csv', 'r') as salary_file:
-            for line in salary_file:
                 while line != "":
                     read_salary = csv.reader(salary_file, delimiter=",")
                     employee_id, last, first, salary, commission, dues, pay_meth = line.split(',')
                     emp = Employee(employee_id, last, first, salary, commission, dues, pay_meth)
                     employee.update(eval(salary_file, emp))
                 with open('timecards.csv', 'r') as time_file:
-                    for line in time_file:
                         while line != "":
                             read_time_cards = csv.reader(time_file, delimiter=",")
                             employee_id, start, end, date = line.split(',')
                             emp = Employee(employee_id, start, end, date)
                             employee.update(eval(time_file, emp))
                         with open('receipts.csv', 'r') as receipt_file:
-                            for line in receipt_file:
                                 while line != "":
                                     read_receipts = csv.reader(receipt_file, delimiter=",")
                                     employee_id, last, item, units, cost, total = line.split(',')
                                     emp = Employee(employee_id, last, item, units, cost, total)
                                     employee.update(eval(receipt_file, emp))
 
-def process_payroll(employee_id):
+
+
+''' def process_payroll(employee_id):'''
+
+
 
 
 
